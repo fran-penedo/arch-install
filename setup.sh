00d77b256781efb79de7c7d0443d58aadb0c9247
Francisco Penedo <fran@franpenedo.com>
2022-03-24 01:03:36 +0100

    Update to Mar 2022 arch linux with encrypted disk

--- setup.sh
+++ setup.sh
@@ -4,35 +4,52 @@
 
 #### Installation
 
-# wifi-menu
+# # wifi-menu # no longer included
+# iwctl --passphrase passphrase station device connect SSID
+
+# timedatectl set-ntp true
 
 # lsblk
 
 # parted /dev/sdx print
 # Look for type: msdos or GPT
 
-# If GPT use cgdisk, if msdos use cfdisk
 # use ntfsresize to resize windows partition
+# ntfsresize -i
+# ntfsresize -s 80G -n /dev/nvme0n1
+# If GPT use cgdisk, if msdos use cfdisk
+
+# If setting up full disk encryption:
+# cryptsetup -y -v --pbkdf pbkdf2 luksFormat /dev/sda2
+# cryptsetup open /dev/sda2 root
+# mkfs.ext4 /dev/mapper/root
+# mount /dev/mapper/root /mnt
 
 # Create filesystems with mkfs.*
-
-# mkswap /dev/sdxY
-# swapon /dev/sdxY
 
 # mount /dev/sdxR /mnt
 # mount everything else. Mount efi partition to /boot/efi if necessary
 
-# change mirrorlist if needed
+# dd if=/dev/zero of=/mnt/swapfile bs=1M count=20480 status=progress
+# chmod 600 /mnt/swapfile
+# mkswap /mnt/swapfile # /dev/sdxY
+# swapon /mnt/swapfile # /dev/sdxY
 
-# pacstrap -i /mnt base base-devel
+# reflector --save /etc/pacman.d/mirrorlist --country Spain --protocol https --latest 5 --sort rate
+
+# pacstrap -i /mnt base base-devel linux linux-firmware zsh fish vi vim man-db man-pages texinfo networkmanager
 
 # genfstab -U -p /mnt >> /mnt/etc/fstab
 
 # arch-chroot /mnt /bin/bash
 
-# Install dialog and wpa-suplicant for easy wifi configuration in case something goes wrong after reboot
+# if encrypted, modify /etc/mkinitcpio.conf
+# HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt filesystems fsck)
+# mkinitcpio -P
 
 # Set root password with passwd
+
+# systemctl enable NetworkManager
 
 #### GRUB installation
 
@@ -45,11 +62,40 @@
 
 ## UEFI (not tested yet)
 
-# Install grub efibootmgr and os-prober
+# Install grub efibootmgr os-prober intel-ucode
+
+# edit /etc/default/grub and uncomment
+# GRUB_DISABLE_OS_PROBER=false
+
+# if encrypted
+# GRUB_ENABLE_CRYPTODISK=y
+
+# add kernel parameters:
+# remove quiet and splash
+
+# if encrypted
+# cryptdevice=UUID=[device-UUID]:root
+# root=/dev/mapper/root
+# resume=/dev/mapper/root
+# resume_offset=[offset] (get it from filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}')
 
 # grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub --recheck
+
+# if encrypted
+# create /root/grub-pre.cfg with:
+
+#set crypto_uuid=[uuid without hyphens]
+#cryptomount -u $crypto_uuid
+## Replace crypto0 by lvm/NameOfVolume if you use LVM
+#set root=crypto0
+#set prefix=($root)/boot/grub
+#insmod normal
+#normal
+
+# grub-mkimage -p /boot/grub -O x86_64-efi -c /root/grub-pre.cfg -o /tmp/grubx64.efi luks2 part_gpt cryptodisk gcry_rijndael pbkdf2 gcry_sha256 ext2
+# install -v /tmp/grubx64.efi esp/EFI/GRUB/grubx64.efi
+
 # grub-mkconfig -o /boot/grub/grub.cfg
-# TODO fix windows not being added to list
 
 #### Post reboot
 
@@ -59,9 +105,12 @@ echo LANG=en_US.UTF-8 > /etc/locale.conf
 export LANG=en_US.UTF-8
 
 ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
+hwclock --systohc
 
 echo fpc > /etc/hostname
 echo Add fpc at the end of all lines
+# 127.0.0.1	localhost.localdomain	localhost fpc
+# ::1		localhost.localdomain	localhost fpc
 read
 nano /etc/hosts
 
